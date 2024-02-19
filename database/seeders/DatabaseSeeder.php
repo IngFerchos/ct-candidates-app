1c518482c5b2804e5f4cd94118ab84e2d4c328b6
JoSebas007 <joangutierrez2001@gmail.com>
2024-02-19 15:53:42 -0500

    created the views and functionalities for the to-do list view

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Task::factory()->count(10)
+            ->sequence(fn (Sequence $sequence) => [ 'order' => $sequence->index+1 ])
+            ->create();
     }
 }
---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-app>
+    <!-- <v-app-bar :elevation="2" color="primary">
+            <template v-slot:prepend>
+                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            </template>
+            <v-app-bar-title>Application Bar</v-app-bar-title>
+        </v-app-bar> -->
+        <v-main>
+            <router-view></router-view>
+        </v-main>
+    </v-app>
+</template>
+<style scoped>
+</style>
