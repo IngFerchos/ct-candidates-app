2964665b88be3249010b515858cde8675c9b1993
Ronny Garcia <rsgarcia@espol.edu.ec>
2024-02-19 12:09:45 -0500

    Add fontawesome icons and component

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,18 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import the fontawesome icons */
+import { faXmark, faEdit, faPlus, faCheck, faSun, faMoon, faHome, faGripVertical } from "@fortawesome/free-solid-svg-icons";
+
+/* add the icons to the library */
+library.add(faXmark, faEdit, faPlus, faCheck, faSun, faMoon, faHome, faGripVertical);
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
@@ -14,7 +26,8 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
+            .component("fa-icon", FontAwesomeIcon)
+            .use(ZiggyVue)            
             .mount(el);
     },
     progress: {
